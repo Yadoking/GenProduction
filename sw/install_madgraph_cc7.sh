8b2cd565cc3dd97eadae0d6ad1930c42311002b9
Junghwan Goh <jhgoh@cern.ch>
2018-09-18 01:46:11 +0900

    cleanup

--- sw/install_madgraph_cc7.sh
+++ sw/install_madgraph_cc7.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 BASEURL="https://raw.githubusercontent.com/Yadoking/GenProduction/master"
-MGURL="https://launchpad.net/mg5amcnlo/2.0/2.6.x/+download/MG5_aMC_v2.6.3.2.tar.gz"
-MG=MG5_aMC_v2_6_3_2
+#MGURL="https://launchpad.net/mg5amcnlo/2.0/2.6.x/+download/MG5_aMC_v2.6.3.2.tar.gz"
+#MG=`basename $MGURL | sed -e 's;.tar.gz;;' -e 's;\.;_;g'`
+MG=$1
 WORKDIR=`pwd`
 
 ### For the users who don't want git-clone
@@ -13,12 +14,13 @@ WORKDIR=`pwd`
 #    done
 #fi
 
-## Download packages
-[ -f `basename $MGURL` ] || wget $MGURL
-tar xzf `basename $MGURL`
 MG=`readlink -f $MG`
 
 cd $MG
+
+## Set default MG5 configuration
+echo "fortran_compiler = gfortran" >> input/mg5_configuration.txt
+echo "text_editor = vim" >> input/mg5_configuration.txt
 
 ## Install necessary components
 bin/mg5_aMC <<EOF
@@ -30,8 +32,8 @@ EOF
 
 ## Download lhapdf6 PDFSet(s)
 ## >> Please add other PDFSets if you think they are necessary
-cd $MG/HEPTools/lhapdf6
-bin/lhapdf get NNPDF23_lo_as_0130_qed
+#cd $MG/HEPTools/lhapdf6
+#bin/lhapdf get NNPDF23_lo_as_0130_qed
 
 ### For the HYU HTOP SGE cluster
 #\cp $WORKDIR/patch/cluster.py $MG/madgraph/various/
